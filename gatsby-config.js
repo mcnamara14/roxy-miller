49339c1539e94522f72359fc3a4206cfb75a9134
Tyler McNamara <tyler.mcnamara@housecallpro.com>
2019-12-07 15:45:55 -0800

    update file name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 const path = require("path")
-const roxyMillerTheme = require("./src/constants/roxyMillerTheme.js")
+const roxyMillerTheme = require("./src/constants/roxyMillerTheme")
 
 module.exports = {
   siteMetadata: {
